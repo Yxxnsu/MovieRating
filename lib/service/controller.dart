44af9bf871f6a7d0a26cce7179401e66dfc5fb28
Yxxnsu <dustn0121@naver.com>
2021-07-19 23:43:50 +0900

    [EDIT] controller Func

--- lib/service/controller.dart
+++ lib/service/controller.dart
@@ -2,29 +2,53 @@ import 'package:get/get.dart';
 import 'package:movie_app/model/movie.dart';
 import 'package:movie_app/model/repository.dart';
 
-class stateController extends GetxController{
-
+class stateController extends GetxController {
   int isSelctedIndex = 0;
   List<Movie> movieList = MovieRepo().loadData();
-  
-  selectedPage(int value){
+  List<Comment> commentList = []; // Class 내의 변수와 변수명이 같아서는 안된다..!
+  List<Comment> temp = [];
+
+  selectedPage(int value) {
     isSelctedIndex = value;
     update();
   }
 
-  dynamic sortRating(){
-    movieList.sort((b,a){
+  sortRating() {
+    movieList.sort((b, a) {
       return a.reservationRate.compareTo(b.reservationRate);
     });
     update();
   }
 
-   dynamic sortDating(){
-     movieList.sort((b,a){
+  sortDating() {
+    movieList.sort((b, a) {
       return a.date.compareTo(b.date);
     });
     update();
   }
 
+  sortGrading(){
+     movieList.sort((a, b) {
+      return a.reservationGrade.compareTo(b.reservationGrade);
+    });
+    update();
+  }
 
+  addComment(String id, String? comment, String? nickName, {double ratings=0}) {    
+    commentList.add(
+      Comment(
+        movieId: id,
+        dateTime: DateTime.now().millisecondsSinceEpoch,
+        comment: comment!,
+        nickName: nickName!,
+        rating: ratings,
+      )
+    );    
+    update();
+  }
+
+  removeComment(Comment value){
+    commentList.remove(value);
+    Future.delayed(Duration(milliseconds: 30),(){update();});
+  }
 }
